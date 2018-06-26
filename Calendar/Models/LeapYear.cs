213e1c888f559d3cfd52c30dd63d255021b4a98b
Elly Maimon <eliranmaimon@gmail.com>
2018-06-26 06:51:34 -0700

    add enclosing folders

---
+++ Calendar/Models/LeapYear.cs
@@ -0,0 +1,17 @@
+namespace Calendar
+{
+  public class LeapYear
+  {
+    public bool IsLeapYear(int year)
+    {
+      if (year % 100 == 0)
+      {
+        return false;
+      }
+      else
+      {
+        return year % 4 == 0;
+      }
+    }
+  }
+}
